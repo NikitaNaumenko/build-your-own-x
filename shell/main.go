e675b781d122fbf74199addf435fa07f1a0a3782
Nikita Naumenko <naumenkoniki@gmail.com>
2021-12-14 18:44:49 +0300

    implement simple shell on go

---
+++ shell/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Print("> ")
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+
+		if err = execute(input); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+	}
+}
+
+var NoPathError = errors.New("Path required")
+
+func execute(input string) error {
+	input = strings.TrimSuffix(input, "\n")
+	args := strings.Split(input, " ")
+	switch args[0] {
+	case "cd":
+		if len(args) < 2 {
+			return NoPathError
+		}
+		return os.Chdir(args[1])
+	case "exit":
+		os.Exit(0)
+	}
+	cmd := exec.Command(args[0], args[1:]...)
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+
+	return cmd.Run()
+}
